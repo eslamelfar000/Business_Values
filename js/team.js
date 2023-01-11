2f7e283612baa43e4482ad3a5fb9006c6c2d7a80
Eslam Elfar <eslamsaber707@gmail.com>
2023-01-11 20:08:31 +0200

    well

---
+++ js/team.js
@@ -0,0 +1,27 @@
+let popUp = document.querySelector('#popUp')
+let popUpContent = document.querySelector('.popUp-content')
+
+const popUpPage = () => {
+    popUp.style.visibility = "visible"
+    popUpContent.style.marginTop = "-100px"
+    popUpContent.style.opacity = "1"
+    document.body.style.overflow = "hidden"
+}
+
+const closePopUpPage = () => {
+    popUp.style.visibility = "hidden"
+    popUpContent.style.marginTop = "0px"
+    document.body.style.overflowY = "auto"
+    popUpContent.style.opacity = "0"
+
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target.className === "popUp-over") {
+        popUp.style.visibility = "hidden"
+        popUpContent.style.marginTop = "0px"
+        popUpContent.style.opacity = "0"
+        document.body.style.overflowY = "auto"
+    }
+})
+
